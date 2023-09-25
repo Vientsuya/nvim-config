f31a5da50455bc4c8b5e18edbb069df9c1a900f9
Marcin Dzierwa <37550706+Vientsuya@users.noreply.github.com>
2023-09-25 15:24:37 +0200

    update

--- lua/custom/plugins/chatgpt.lua
+++ lua/custom/plugins/chatgpt.lua
@@ -3,7 +3,7 @@ return {
   event = "VeryLazy",
   config = function()
     require("chatgpt").setup({
-      api_key_cmd = "echo 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"
+      api_key_cmd = "echo "
     })
   end,
   dependencies = {
